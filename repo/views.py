90bf4750b07cc26167a98618c22f08fc3b34cd49
abhaykoduru <10239366+abhaykoduru@users.noreply.github.com>
2021-10-31 20:29:06 -0500

    Add questionnaire listcreate api

--- repo/views.py
+++ repo/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.permissions import IsAuthenticated
+from .serializers import QuestionnaireSerializer
+from .models import Questionnaire
 
-# Create your views here.
+
+class QuestionnaireView(ListCreateAPIView):
+    queryset = Questionnaire.objects.all()
+    serializer_class = QuestionnaireSerializer
+    permission_classes = (IsAuthenticated,)
