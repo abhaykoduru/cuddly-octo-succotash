cf3a1573a652e94369e02a38792fff182dd5f548
abhaykoduru <10239366+abhaykoduru@users.noreply.github.com>
2021-10-31 20:20:08 -0500

    Setup jwt authentication

--- docrepo/settings.py
+++ docrepo/settings.py
@@ -40,8 +40,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'repo'
 ]
+
+REST_FRAMEWORK = {
+'DEFAULT_AUTHENTICATION_CLASSES': (
+'rest_framework_simplejwt.authentication.JWTAuthentication',
+   )
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
